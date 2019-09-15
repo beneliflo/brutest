1e583a7e4610df4097bd0368a8ca3e79faac4be6
Benjamin Flores <beneliflo@pixel2html.net>
2019-09-15 00:39:34 -0300

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,41 @@
 module.exports = {
+  pathPrefix: '/movie-library-with-react',
   siteMetadata: {
-    title: `brutest`,
-    description: `Search best movies`,
-    author: `@beneliflo`,
+    title: 'movie-library',
+    description: 'Search best movies',
+    author: '@beneliflo',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'images',
+        path: 'src/images',
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: 'gatsby-plugin-prefetch-google-fonts',
       options: {
         fonts: [
           {
-            family: `Lato`,
-            subsets: [`latin`],
-            variants: [`400`, `700`, `900`]
+            family: 'Lato',
+            subsets: ['latin'],
+            variants: ['400', '700', '900']
           },
         ],
       },
@@ -49,6 +50,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // 'gatsby-plugin-offline',
   ],
 }
